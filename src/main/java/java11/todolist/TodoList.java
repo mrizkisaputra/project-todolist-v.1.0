a60ccfd03abb87f62de6bc19ac5d4f0192ebbc79
mrizkisaputra <mrizkisaputra6@gmail.com>
2021-08-06 23:28:11 +0700

    adding color terminal

--- src/main/java/java11/todolist/TodoList.java
+++ src/main/java/java11/todolist/TodoList.java
@@ -4,6 +4,20 @@ package java11.todolist;
 import java.util.Scanner;
 
 public class TodoList {
+  private static final String RESET,RED,GREEN,PURPLE,BLUE,YELLOW,RED_BOLD,GREEN_BOLD,YELLOW_BOLD;
+  static {
+    RESET = "\033[0m";
+    RED = "\033[0;31m";
+    GREEN = "\033[0;32m";
+    PURPLE = "\033[0;35m";
+    BLUE = "\033[0;34m";
+    YELLOW = "\033[0;33m";
+    RED_BOLD = "\033[1;31m";
+    GREEN_BOLD = "\033[1;32m";
+    YELLOW_BOLD = "\033[1;33m";
+  }
+
+
   private static final Scanner scan = new Scanner(System.in);
   private static boolean stop = true;
   public static String[] modelDataTodoList = new String[10];
@@ -19,7 +33,7 @@ public class TodoList {
       var number = i+1;
 
       /*jika setiap elemen todolist tidak null maka tampilkan*/
-      if (modelDataTodoList[i] != null) System.out.println(number+". "+todo);
+      if (modelDataTodoList[i] != null) System.out.println(PURPLE+number+". "+todo+RESET);
     }
   }
 
@@ -102,16 +116,16 @@ public class TodoList {
    */
   public static void viewMainTodoList() {
     while (stop) {
-      System.out.println("-----------+ Todo List +-----------");
+      System.out.println(YELLOW_BOLD+"-----------+ Todo List +-----------"+RESET);
       showTodoList();
-      System.out.println("-----------------------------------");
-      System.out.println("-------------+ Menu +--------------");
-      System.out.print("[1]. Add TodoList\n" +
-              "[2]. Delete TodoList\n" +
-              "[3]. Update TodoList\n" +
-              "[x]. Exit\n");
-      System.out.println("-----------------------------------");
-      System.out.print("Choose Menu > ");
+      System.out.println(YELLOW_BOLD+"-----------------------------------"+RESET);
+      System.out.println(YELLOW_BOLD+"-------------+ Menu +--------------"+RESET);
+      System.out.print(GREEN+"[1]. Add TodoList\n"+RESET +
+              GREEN+"[2]. Delete TodoList\n"+RESET +
+              GREEN+"[3]. Update TodoList\n"+RESET +
+              GREEN+"[x]. Exit\n"+RESET);
+      System.out.println(YELLOW_BOLD+"-----------------------------------"+RESET);
+      System.out.print(YELLOW_BOLD+"Choose Menu > "+RESET);
       String choose = scan.nextLine();
 
       switch (choose) {
@@ -126,7 +140,7 @@ public class TodoList {
 
 
   public static void viewAddTodoLits() {
-    System.out.print("input name todolist : ");
+    System.out.print(GREEN_BOLD+"input name todolist : "+RESET);
     String todo = scan.nextLine();
     addTodoList(todo);
     System.out.println("\n\n");
@@ -134,7 +148,7 @@ public class TodoList {
 
 
   public static void viewDeleteTodoList() {
-    System.out.print("delete number todolist (x if cancel) : ");
+    System.out.print(RED_BOLD+"delete number todolist (x if cancel) : "+RESET);
     String deleteTodo = scan.nextLine();
 
     if (deleteTodo.equalsIgnoreCase("x")) {
@@ -142,26 +156,26 @@ public class TodoList {
     }
     else {
       var success = deleteTodoList(Integer.valueOf(deleteTodo));
-      if (!success) System.out.println("Fail Delete Number! "+deleteTodo+" Not Found\n\n");
-      if (success) System.out.println("Success Delete Todo\n\n");
+      if (!success) System.out.println(RED+"Fail Delete Number! "+deleteTodo+" Not Found\n\n"+RESET);
+      if (success) System.out.println(BLUE+"Success Delete Todo\n\n"+RESET);
     }
 
   }
 
 
   public static void viewUpdateTodoList() {
-    System.out.print("input new name todolist (x if cancel) : ");
+    System.out.print(GREEN_BOLD+"input new name todolist (x if cancel) : "+RESET);
     String newNameTodo = scan.nextLine();
 
     if (newNameTodo.equalsIgnoreCase("x")) {
       stop = false;
     } else {
-      System.out.print("input update number todolist : ");
+      System.out.print(GREEN_BOLD+"input update number todolist : "+RESET);
       String updateNumberTodo = scan.nextLine();
 
       boolean success = updateTodoList(Integer.valueOf(updateNumberTodo), newNameTodo);
-      if (!success) System.out.println("Update Todo Fail! Number "+updateNumberTodo+" Not Found!\n\n");
-      if (success) System.out.println("Update Todo Success\n\n");
+      if (!success) System.out.println(RED+"Update Todo Fail! Number "+updateNumberTodo+" Not Found!\n\n"+RESET);
+      if (success) System.out.println(BLUE+"Update Todo Success\n\n"+RESET);
     }
   }
 /*===========================================END: Flow View===========================================*/
